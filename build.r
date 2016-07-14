d87af514f76021e38dc329ca957a6fc83b588185
Michio Oguro <michio_oguro@hotmail.com>
2016-07-14 14:14:56 +0900

    Standardize format of build script. ビルドスクリプトのフォーマットを標準化した。

--- build.r
+++ build.r
@@ -3,16 +3,16 @@ require(devtools)
 
 
 get.this.file.dir <- function() {
-   	cmdArgs <- commandArgs(trailingOnly = FALSE)
-   	needle <- "--file="
-   	match <- grep(needle, cmdArgs)
-   	if (length(match) > 0) {
-   		# Rscript
-   		return(dirname(sub(needle, "", cmdArgs[match])))
-   	} else {
-   		# 'source'd via R console
-   		return(dirname(normalizePath(sys.frames()[[1]]$ofile)))
-   	}
+	cmdArgs <- commandArgs(trailingOnly = FALSE)
+	needle <- "--file="
+	match <- grep(needle, cmdArgs)
+	if (length(match) > 0) {
+		# Rscript
+		return(dirname(sub(needle, "", cmdArgs[match])))
+	} else {
+		# 'source'd via R console
+		return(dirname(normalizePath(sys.frames()[[1]]$ofile)))
+	}
 }
 
 setwd(get.this.file.dir())
