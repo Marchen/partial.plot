999b507473c87b590b2fcd5fd0b928db3a6f1153
Michio Oguro <michio_oguro@hotmail.com>
2017-02-17 13:43:30 +0900

    Using residuals() method of model.adapter, updated the way of calculation of partial residuals. model.adapterのresiduals()メソッドを使って、偏残差計算方法を単純化した。

--- R/partial.residual.r
+++ R/partial.residual.r
@@ -52,55 +52,27 @@ partial.residual$methods(
 #------------------------------------------------------------------------------
 #	偏残差を計算する。
 #------------------------------------------------------------------------------
-#	PL      = i + PW + SL + SP + PW:SP + SL:SP + r		# FULL MODEL
-#	PL      = i + PW      + SP + PW:SP         + r		# Want to see this
-#	PL      =        + SL              + SL:SP		    # Remove these effects
-#	predict = i + PW + SL + SP + PW:SP + SL:SP
-#
-#	Case of LSMEANS
-#	LSMEANS = i + PW + MM + SP + PW:SP + MM:SP
-#	LSMEANS = i + PW + MM + SP + PW:SP + MM:SP + r		# Want to see this
-#------------------------------------------------------------------------------
 partial.residual$methods(
 	calculate.residuals = function() {
 		"
 		Calculate partial residuals.
 		"
-		# Prepare names of numeric variables.
-		# 数値型変数の変数名を用意。
-		all.numerics <- settings$adapter$x.names(type = "base")
-		all.numerics <- all.numerics[
-			sapply(settings$data[all.numerics], is.numeric)
-		]
-		other.numerics <- all.numerics[
-			!all.numerics %in% settings$x.names.numeric
-		]
-		# Calculate prediction 1.
-		# 予測値１を計算。
-		data1 <- settings$data
-		for (i in other.numerics) {
-			data1[[i]] <- data1[[i]] - mean(data1[[i]])
+		# Prepare data for partial residual.
+		newdata <- settings$data
+		for (i in settings$adapter$x.names(type = "base")) {
+			if (is.numeric(newdata[[i]]) & !i %in% settings$x.names.numeric) {
+				newdata[[i]] <- mean(newdata[[i]], na.rm = TRUE)
+			}
 		}
-		data1[settings$x.names.numeric] <- 0
-		pred1 <- settings$adapter$predict(newdata = data1, type = "link")
-		pred1 <- pred1$fit[, "fit"]
-		# Calculate prediction 2.
-		# 予測値２を計算。
-		data2 <- settings$data
-		data2[all.numerics] <- 0
-		pred2 <- settings$adapter$predict(newdata = data2, type = "link")
-		pred2 <- pred2$fit[, "fit"]
 		# Calculate partial residual.
-		# 偏残差を計算。
-		result <- (
-			settings$adapter$link(settings$data[[settings$adapter$y.names()]])
-			- (pred1 - pred2)
-		)
+		pred <- settings$adapter$predict(newdata = newdata, type = "link")
+		residual <- pred$fit[, "fit"] + settings$adapter$residuals("link")
 		if (settings$type == "response") {
-			result <- settings$adapter$linkinv(result)
+			residual <- settings$adapter$linkinv(residual)
 		}
-		.self$data <- result
+		.self$data <- residual
 	}
 )
+
 
 
