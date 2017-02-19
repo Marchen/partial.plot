9957299067fd759fc20382008bc8ada654c0c201
Michio Oguro <michio_oguro@hotmail.com>
2017-02-19 15:40:52 +0900

    Because MCMCglmm produce strange plot using the new way of calculation about partial residuals, the old way was restored and now partial.plot() use either way depending on the function. MCMCglmmが新しい偏残差の計算方法で変な値を出すので、古い計算方法を復旧させて、関数に応じて使い分けるようにした。

--- R/partial.residual.r
+++ R/partial.residual.r
@@ -42,7 +42,14 @@ partial.residual$methods(
 			)
 			warning(message)
 		} else {
-			.self$calculate.residuals()
+			lsmeans.compatible <- !any(
+				class(settings$model) %in% LSMEANS_INCOMPATIBLE_MODELS
+			)
+			if (lsmeans.compatible) {
+				.self$calculate.residuals.lsmeans()
+			} else {
+				.self$calculate.residuals()
+			}
 			settings$set.residual(.self)
 		}
 	}
@@ -51,6 +58,37 @@ partial.residual$methods(
 
 #------------------------------------------------------------------------------
 #	偏残差を計算する。
+#------------------------------------------------------------------------------
+#	Assume that creating a model predicts petal length (PL) by petal width (PW)
+#	and sepal length (SL) and species (SP) and their interactions.
+#		i: intercept			r: residual
+#
+#	PL      = i + PW + SL + SP + PW:SP + SL:SP + r		# Response variable
+#	PL      = i + PW + SL + SP + PW:SP + SL:SP			# FULL MODEL
+#	PL      =                                  + r		# Residual
+#	predict = i + PW + SL + SP + PW:SP + SL:SP			# Result of predict()
+#
+#	To see the partial relationship between PL and SL...
+#
+#	If we want to plot contrast plot, partial residual is:
+#	PL      = i + PW    0 + SP + PW:SP       0 + r
+#
+#	To see this, we need to remove:
+#	PL      =        + SL              + SL:SP		    # Remove these effects
+#
+#	lsmeans() calculates:
+#	LSMEANS = i + MM + SL + SP + MM:SP + SL:SP
+#		where MM is mean of petal width (PW).
+#
+#	So when using lsmeans(), i.e., conditional plot, partial residual is:
+#	LSMEANS = i + MM + SL + SP + MM:SP + SL:SP + r
+#
+#	This can be achieved using predict() with using mean values for
+#	petal width (PW) and adding residual for it.
+#	However, becase MCMCglmm show deviated values from this calculation,
+#	currently partial residual for MCMCglmm (and models compatible with
+#	lsmeans) are calculated by special method.
+#
 #------------------------------------------------------------------------------
 partial.residual$methods(
 	calculate.residuals = function() {
@@ -61,7 +99,8 @@ partial.residual$methods(
 		newdata <- settings$data
 		for (i in settings$adapter$x.names(type = "base")) {
 			if (is.numeric(newdata[[i]]) & !i %in% settings$x.names.numeric) {
-				newdata[[i]] <- mean(newdata[[i]], na.rm = TRUE)
+				# Set median for other numeric parameters.
+				newdata[[i]] <- median(newdata[[i]], na.rm = TRUE)
 			}
 		}
 		# Calculate partial residual.
@@ -75,4 +114,49 @@ partial.residual$methods(
 )
 
 
+#------------------------------------------------------------------------------
+#	偏残差を計算する。
+#------------------------------------------------------------------------------
+
+partial.residual$methods(
+	calculate.residuals.lsmeans = function() {
+		"
+		Calculate partial residuals.
+		"
+		# Prepare names of numeric variables.
+		# 数値型変数の変数名を用意。
+		all.numerics <- settings$adapter$x.names(type = "base")
+		all.numerics <- all.numerics[
+			sapply(settings$data[all.numerics], is.numeric)
+		]
+		other.numerics <- all.numerics[
+			!all.numerics %in% settings$x.names.numeric
+		]
+		# Calculate prediction 1.
+		# 予測値１を計算。
+		data1 <- settings$data
+		for (i in other.numerics) {
+			data1[[i]] <- data1[[i]] - mean(data1[[i]])
+		}
+		data1[settings$x.names.numeric] <- 0
+		pred1 <- settings$adapter$predict(newdata = data1, type = "link")
+		pred1 <- pred1$fit[, "fit"]
+		# Calculate prediction 2.
+		# 予測値２を計算。
+		data2 <- settings$data
+		data2[all.numerics] <- 0
+		pred2 <- settings$adapter$predict(newdata = data2, type = "link")
+		pred2 <- pred2$fit[, "fit"]
+		# Calculate partial residual.
+		# 偏残差を計算。
+		result <- (
+			settings$adapter$link(settings$data[[settings$adapter$y.names()]])
+			- (pred1 - pred2)
+		)
+		if (settings$type == "response") {
+			result <- settings$adapter$linkinv(result)
+		}
+		.self$data <- result
+	}
+)
 
